bceb05a3a794e5cda4d73bb52ad47b25bbfb83e5
juan <cruz59349@gmail.com>
2023-10-25 17:11:43 +0000

    This is a script that uses functions to print the login history of my ubuntu server.

---
+++ ops-challenge-functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:					ops-challenge-functions
+# Author: 					Juan Maldonado
+# Date of latest revision:			10/24/2023
+# Purpose:					ops challenge - using functions.
+
+# Declaration of variables 
+
+# Declaration of functions
+function login_histroy () {
+    echo This is the login history:
+    echo
+    last
+}
+# Main
+login_histroy
+login_histroy
+login_histroy
+
+# End 
