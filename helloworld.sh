8c21c8e0ffad53d8171691aa0ea05d40da8b2893
juan <cruz59349@gmail.com>
2023-10-25 00:22:20 +0000

    This is my ops challenge assignment - My first bash script

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script Name:              hellowordl.sh
+# Author:                   juan maldonado
+# Date of latest revision:  10/24/2023
+# Purpose:                  Ops challenge - My first bash script
+
+# Declaration of variables
+MESSAGE="I am working perfectly!"
+
+# Declaration of functions
+
+# Main
+echo "$MESSAGE"
+
+# End
