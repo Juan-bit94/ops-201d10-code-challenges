eb5934c6ba59ad8cf85368130219cf29e7c262ea
juan <cruz59349@gmail.com>
2023-10-26 15:47:46 +0000

    This version puts the code to make files and directories within functions

--- ops-challenge-arrays.sh
+++ ops-challenge-arrays.sh
@@ -6,27 +6,35 @@
 # Purpose:					    ops challenge - using arrays.
 
 
-# Declaration of functions
+# Declaration of variables 
 
 # This creates four strings within an array
 list_of_directories=("dir1" "dir2" "dir3" "dir4")
 
+# This variable holds number of directories 
+NumberOfDirectories=${#list_of_directories[@]}
+
+# Declaration of functions
+
 # This creates a directory for each element in the array.
-for dir in "${list_of_directories[@]}"; do
+directory_maker () {
+    for dir in "${list_of_directories[@]}"; do
     mkdir -p "$dir"
 done
+}
 
 # This creates a .txt file in each directory
-for dir in "${list_of_directories[@]}"; do
+file_maker () {
+    for dir in "${list_of_directories[@]}"; do
     file_name="$dir/file1.txt"
     touch "$file_name"
 done
-
-# Declaration of variables 
-NumberOfDirectories=${#list_of_directories[@]}
+}
 
 # Main
 
+directory_maker
+file_maker
 echo "$NumberOfDirectories Directories and .txt files created successfully"
 
 # End 
