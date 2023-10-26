5ba358baf5a420773fd5449f4ed3483ca793c781
juan <cruz59349@gmail.com>
2023-10-26 06:15:49 +0000

    This is a modified bash script, added code to state how many dir were created

--- ops-challenge-arrays.sh
+++ ops-challenge-arrays.sh
@@ -5,14 +5,11 @@
 # Date of latest revision:		10/25/2023
 # Purpose:					    ops challenge - using arrays.
 
-# Declaration of variables 
 
-num_of_directories=${#list_of_directories[@]}
+# Declaration of functions
 
 # This creates four strings within an array
 list_of_directories=("dir1" "dir2" "dir3" "dir4")
-
-# Declaration of functions
 
 # This creates a directory for each element in the array.
 for dir in "${list_of_directories[@]}"; do
@@ -21,12 +18,15 @@ done
 
 # This creates a .txt file in each directory
 for dir in "${list_of_directories[@]}"; do
-    file_name="$dir/newfile.txt"
+    file_name="$dir/file1.txt"
     touch "$file_name"
 done
 
+# Declaration of variables 
+NumberOfDirectories=${#list_of_directories[@]}
+
 # Main
 
-echo "$num_of_directories Directories and .txt files created successfully"
+echo "$NumberOfDirectories Directories and .txt files created successfully"
 
 # End 
