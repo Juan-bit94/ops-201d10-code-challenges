e3a9d1261fb1f3daaf230324dde1f431522eb7fe
juan maldonado <cruz59349@gmail.com>
2023-10-30 06:10:49 +0000

    This is for my conditionals code challenge

--- ops-challenge-conditionals.sh
+++ ops-challenge-conditionals.sh
@@ -1,52 +1,45 @@
 #!/bin/bash
 
-# Script Name:				ops-challenge-conditionals
-# Author:				juan maldonado
-# Date of latest revision:		10/29/2023
-# Purpose:				A directory checker and maker
-
+# Script Name:					ops-challenge-loops
+# Author:				        Juan Maldonado
+# Date of latest revision:		10/27/2023
+# Purpose:				    	ops-challenge-conditionals code assignment 
 
 # Declaration of variables
-
 introMessage="Welcome to the directory checker program."
 userPrompt="Enter a directory you want to check for: "
 list_of_directories=("dir1" "dir2" "dir3")
 
 # Declaration of functions
-
-function create_directory () {
-	read -p "Enter a name for a new directory: " userInput
-	mkdir "$userInput"
-	echo "Created directory: $userInput"
-	echo "Now $userInput will be added to the list of directories"
-	list_of_directories+=("$userInput")
+create_directory() {
+    read -p "Enter a name for a new directory: " userInput
+    mkdir "$userInput"
+    echo "Created directory: $userInput"
+    echo "Now $userInput will be added to the list of directories"
+    list_of_directories+=("$userInput")
+	echo " Here are the directories we have on file: " 
+	for dir in "${list_of_directories[@]}"; do
+                echo "$dir"
+done
 }
-function check_directory () {
 
-echo "$introMessage"
-read -p "$userPrompt" input1
-
-# check if the directory exists
-# Use a for loop to check user input in array
-for dir in "${list_of_directories[@]}"; do
-	if [ "$input1" == "$dir" ]; then
-		echo "$input1 already exists" 
-		read -p "Since the directory exist, would you like to create a new directory?[Y/n]: " input2
-	
+check_directory() {
+    echo "$introMessage"
+    read -p "$userPrompt" input1
+	echo "here are the directories we have: "
+	for dir in "${list_of_directories[@]}"; do
+                echo "$dir"
+            done
+	read -p "Does your request match our list?[Y/n]: " input2
 		if [ "$input2" == "Y" ]; then
-		create_directory
-	else
-		echo "before you go, here is the list of the directories we have: "
-		for dir in "${list_of_directories[@]}"; do
-			echo "$dir"
-			echo "Script will exit now."
-			break
-		
-done 
+			echo "Great! Happy to help."
+		else 
+			echo "That's okay, lets create the directory and add it to our list"
+			create_directory
+		fi 
 }
 
 # Main
-
 check_directory
 
 # End
