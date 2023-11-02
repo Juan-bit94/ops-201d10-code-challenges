83e9b1ea2436f957116c23e4f5e7b46d281ec54b
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2023-11-02 14:56:00 -0700

    Add files via upload
    This is for my ops-201-code-challenge- for powershell

---
+++ log_retrival.ps1
@@ -0,0 +1,29 @@
+# Script Name:				ops-challenge-Log-Retrieval
+# Author:					Juan Maldonado
+# Date of lastest revision: 11/2/2023
+# Purpose:					ops-challenge-conditionals-code assignment
+
+# Declaration of variables
+
+# 
+$last24Path = "C:\Users\Administrator02\Desktop\logs\24log.txt.txt"
+$errorsPath = "C:\Users\Administrator02\Desktop\logs\errorlog.txt.txt"
+
+# Clear the existing contents of the log files
+Clear-Content $last24Path
+Clear-Content $errorsPath
+
+# Retrieves all events from system log in the last 24 hours and sends it to last_24_report
+Get-WinEvent -LogName System -FilterXPath "*[System[(EventID=16) and TimeCreated[timediff(@SystemTime) <= 86400000]]]" | Format-List | Out-File -FilePath $last24Path -Append
+
+# Retrieves all error type events and puts it in the errors_report.txt
+Get-WinEvent -LogName System -ErrorAction SilentlyContinue | Where-Object { $_.LevelDisplayName -eq "Error" } | Format-List | Out-File -FilePath $errorsPath -Append
+
+# Prints to the screen events with ID 16
+Get-WinEvent -LogName System -FilterXPath "*[System[EventID=16]]" | Format-Table -AutoSize
+
+# Prints to screen 20 recent entries 
+Get-WinEvent -LogName System -MaxEvents 20 | select-Object -ExpandProperty ProviderName
+
+# Print to the screen 500 most recent entries
+Get-WinEvent -LogName System -MaxEvents 500 | Select-Object -ExpandProperty ProviderName
