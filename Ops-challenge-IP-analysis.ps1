055f12d44b6d2f479a377b365eaf4965e26319e9
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2023-11-07 09:38:46 -0800

    Add files via upload
    This commit is for the code challenge for IP analysis.

---
+++ Ops-challenge-IP-analysis.ps1
@@ -0,0 +1,40 @@
+﻿# Declare variable
+
+# intro message
+$introMessage = "Welcome to the IP analyzer."
+
+# Declare Function
+
+# Get's user input for the file name
+function Get-FileName {
+    $fileName = Read-Host "Enter a file name and extension "
+    return $fileName
+}
+
+# This runs ipconfig /all and save the output to a file
+function save-IpConfigToFile {
+    $fileName = Get-FileName
+    ipconfig /all | Out-File -FilePath $fileName
+    Write-Host "ipconfig output saved to $fileName."
+}
+
+# This extracts IPv4 address from the file
+function Get-IPv4AddressFromFile($fileName) {
+    $fileContent = Get-Content $fileName
+    $ipv4Addresses = $fileContent | Select-String -Pattern "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" | ForEach-Object { $_.Matches.Value }
+    return $ipv4Addresses
+}
+
+# Main 
+
+Write-Host $introMessage
+# $fileName = Get-FileName
+save-IpConfigToFile
+$ipv4Addresses = Get-IPv4AddressFromFile $fileName
+
+if ($ipv4Addresses.Count -eq 0) {
+    Write-Host "No IPv4 addresses found in $fileName."
+} else {
+    Write-Host "IPv4 addresses found in $fileName : "
+    $ipv4Addresses
+}
