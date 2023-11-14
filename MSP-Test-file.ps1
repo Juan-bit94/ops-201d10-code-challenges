bc0497b7a0a98b98cc7a72923341c9a98d2679d8
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2023-11-14 14:30:51 -0800

    Update MSP-Test-file.ps1

--- MSP-Test-file.ps1
+++ MSP-Test-file.ps1
@@ -1,17 +1,22 @@
+# Script Name:              MSP-Test-file.ps1
+# Author:                   juan maldonado
+# Date of latest revision:  11/14/2023
+# Purpose:                  Endpoint configuration
+
 # This sets up variables
 $UserName = Read-Host "Enter new username"
 $Password = Read-Host -Prompt "Enter the password for $UserName" -AsSecureString
 $FullName = Read-Host "Enter the full name for $UserName"
 $Description = Read-Host "Enter a description for $UserName"
 $ComputerName = Read-Host "Enter a ComputerName"
-$ISOPath = "C:\Users\juan9\OneDrive\Desktop" # this is for the source computer, find the path to the windows 10 iso.
+$ISOPath = "C:\Users\Admin01\Desktop\Windows-ISO\Windows-ISO\Windows10.iso" # this is for the source computer, find the path to the windows 10 iso.
 
 # This installs Windows 10
 # Note: Replace \\Server\Share with the actual network path to your Windows 10 installation files
-$SetupPath = "\\Server\Share\setup.exe"
-Invoke-Command -ComputerName $ComputerName -ScriptBlock {
-    Start-Process -Wait -FilePath $using:SetupPath -ArgumentList "/quiet /noreboot"
-}
+# $SetupPath = "C:\Users\Admin01\Desktop\Windows-ISO"
+# Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+   # Start-Process -Wait -FilePath $using:SetupPath -ArgumentList "/quiet /noreboot"
+#}
 
 # This sets up user account
 New-LocalUser -Name $UserName -Password $Password -FullName $FullName -Description $Description
@@ -22,26 +27,35 @@ Add-LocalGroupMember -Group "Administrators" -Member $UserName
 $TimeZone = Read-Host "Enter the time zone (e.g., Pacific Standard Time)"
 Set-TimeZone -Id $TimeZone
 
+ # This Enables File and Printer Sharing 
+Enable-NetFirewallRule -Name "FPS-SMB-In-TCP"
+
+# This allow ICMP traffic 
+Enable-NetFirewallRule -Name "FPS-ICMP4-ERQ-In"
+
+
 # This Runs Windows Update
-Install-WindowsUpdate -AcceptAll -AutoReboot
+$updates = Get-WindowsUpdate -Online
+if ($updates.Count -gt 0) {
+    # this installs updates
+    Install-WindowsUpdate -Updates $updates -AcceptAll -AutoReboot
+}
 
 # This installs google chrome
-Start-Process -Wait -FilePath "C:\Users\juan9\OneDrive\Desktop\Programs-to-install"
+Start-Process -Wait -FilePath "C:\Users\Admin01\Desktop\Programs-to-install\Programs-to-install\ChromeSetup.exe"
 
 # This installs Thunderbird email
-Start-Process -Wait -FilePath "C:\Users\juan9\OneDrive\Desktop\Programs-to-install"
+Start-Process -Wait -FilePath "C:\Users\Admin01\Desktop\Programs-to-install\Programs-to-install\Thunderbird Setup 115.4.2.exe"
 
 # This installs Slack
-Start-Process -Wait -FilePath "C:\Users\juan9\OneDrive\Desktop\Programs-to-install"
+Start-Process -Wait -FilePath "C:\Users\Admin01\Desktop\Programs-to-install\Programs-to-install\SlackSetup.exe"
 
 # This installs VLC
-Start-Process -Wait -FilePath "C:\Users\juan9\OneDrive\Desktop\Programs-to-install"
+Start-Process -Wait -FilePath "C:\Users\Admin01\Desktop\Programs-to-install\Programs-to-install\vlc-3.0.20-win64.exe"
 
 # This installs Malwarebytes
-Start-Process -Wait -FilePath "C:\Users\juan9\OneDrive\Desktop\Programs-to-install"
+Start-Process -Wait -FilePath "C:\Users\Admin01\Desktop\Programs-to-install\Programs-to-install\MBSetup-5.5.exe"
 
-# This installs AMD Software: Adrenalin Edition
-Start-Process -Wait -FilePath "C:\Users\juan9\OneDrive\Desktop\Programs-to-install"
 
 # This configures security settings
 # Enable Windows Defender
@@ -53,21 +67,15 @@ Enable-NetFirewallRule -Name "FPS-SMB-In-TCP"
 # This allow ICMP traffic 
 Enable-NetFirewallRule -Name "FPS-ICMP4-ERQ-In"
 
-# This enables Remote Managment with PowerShell Remoting
-Enable-PSRemoting -Force
-
-# This sets up file and folder permissions
-# Example: Grant permissions to a folder
-$Folder = "C:\Path\to\Folder"
-$Acl = Get-Acl $Folder
-$Permission = "BUILTIN\Users", "ReadAndExecute", "Allow"
-$Rule = New-Object System.Security.AccessControl.FileSystemAccessRule $Permission
-$Acl.SetAccessRule($Rule)
-Set-Acl -Path $Folder -AclObject $Acl
 
 # This creates a system backup and restore points
 # Example: Create a system restore point
 Checkpoint-Computer -Description "Before Software Installation" -RestorePointType "MODIFY_SETTINGS"
+
+# This disables SMBv1
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+
+Write-Host "Windows 10 configuration complete."
 
 # This will Reboot the computer
 Restart-Computer -Force
