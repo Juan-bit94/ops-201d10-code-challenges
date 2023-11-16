3db734734881ca99da33178dce1be735aff9a45b
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2023-11-15 20:41:14 -0800

    Update Backup-solution-v1.ps1

--- Backup-solution-v1.ps1
+++ Backup-solution-v1.ps1
@@ -3,7 +3,6 @@ $UserName = $env:USERNAME
 Write-Host "Current username: $Username"
 
 # This sets up variables
-$UserName = Read-Host "Enter the username for the account"
 $BackupPath = "C:\Backups\$UserName"
 
 # This creates a backup folder if it doesn't exist
@@ -11,19 +10,19 @@ if (-not(Test-Path $BackupPath -PathType Container)) {
     New-Item -ItemType Directory -Path $BackupPath -Force
 }
 
-# This copies user files to the backup folder
-Copy-Item -Path "C:\users\$UserName\Documents" -Destination $BackupPath -Recurse -Force
+# This uses robocopy to backup files
+$SourcePath = "C:\users\$UserName\Documents"
+robocopy $SourcePath $BackupPath /E /COPY:DAT /R:0 /W:0 
 
 # This creates a system restore point
 $RestorePointDescription = "Backup for $UserName"
 
 # This invokes the Checkpoint-Computer cmdlet using PowerShell
 try {
-    Invoke-Command -ScriptBlock {
-        Checkpoint-Computer -Description $using:RestorePointDescription -RestorePointType MODIFY_SETTINGS
-        } -Credential (Get-Credential) -Authentication Credssp
-        Write-Host "System restore point created successfully."
-        } catch {
-            Write-Host "Failed to create system restore point: $_"
-        }
+     Checkpoint-Computer -Description $RestorePointDescription -RestorePointType MODIFY_SETTINGS
+     Write-Host "System restore point created successfully."
+     } catch {
+         Write-Host "Failed to create system restore point: $_"
+     }
 Write-Host "Backup and checkpoint process completed."
+
